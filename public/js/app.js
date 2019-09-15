e2d07f810a1271ae889555f8ee297bbaf981aa3c
bsviraj <bilayviraj@gmail.com>
2019-09-15 20:49:28 +0530

    removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Client side javascipt loaded!');
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const pLocation = document.getElementById('location');
